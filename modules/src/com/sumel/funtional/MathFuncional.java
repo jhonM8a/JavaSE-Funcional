14ff8141abacf69c9b7e24d9e4677ef71ee743cd
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-04-09 12:22:42 -0500

    Se agrega comentarios y uso de prodicados

--- modules/src/com/sumel/funtional/MathFuncional.java
+++ modules/src/com/sumel/funtional/MathFuncional.java
@@ -1,8 +1,22 @@
 package com.sumel.funtional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
+
 
 public class MathFuncional {
+    //Clase ejemplo para predicados
+    static class Student{
+        private double calificacion;
+
+        public Student(double calificacion){
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
+    }
     public static void main(String[] args) {
         Function<Integer, Integer> square = new Function<Integer, Integer>() {
             @Override
@@ -10,7 +24,35 @@ public class MathFuncional {
                 return num*num;
             }
         };
+        /**
+         * Es la misma función pero mas legible.
+         * Recibe un entero x y le aplica el modulo 2
+         * para saber si es para o impar
+         * */
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+
+        System.out.println(isOdd.apply(3));
         System.out.println(square.apply(5));
+
+        /**
+         * Interface Predicate:
+         * Es una función que trabaja sobre un Tipo
+         * pero genera un boolean, esto testea si algo es valido.
+         * Esto genera validaciones rapidas, beneficia al filtrar elementos
+         * o verificar si algo tiene datos
+         *
+         * */
+        Predicate<Integer> isEven = x -> x%2 ==0;
+        System.out.println("Predicate->isEven: " +isEven.test(4));
+        //Ejemplo de uso para verificar que un estudiante aprobo
+        Predicate<Student> isApproved = student -> student.calificacion>=3.0;
+        System.out.println("Predicate->isApproved: "+isApproved.test(new Student(3.1)));
+
+        /**
+         * Elementos para consumir o proveer datos: Consumer y Supplier
+         *
+         * */
 
     }
 }
