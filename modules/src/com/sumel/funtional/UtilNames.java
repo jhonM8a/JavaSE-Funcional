99a0c0ef6b038f6f0cccaf550d74ab70ed7c30f4
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-04-09 13:51:01 -0500

    Uso de operador de referencia para usar metodos ya existentes sin crear funciones

---
+++ modules/src/com/sumel/funtional/UtilNames.java
@@ -0,0 +1,31 @@
+package com.sumel.funtional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class UtilNames {
+
+    public static void main(String[] args) {
+        List<String> profesores = getList("Nicolas", "Juan", "Jhon");
+
+        Consumer<String> printerProfessorsRedundancy = text-> System.out.println(text);
+        profesores.forEach(printerProfessorsRedundancy);
+        System.out.println("--------------------------------------------------");
+        /**
+         * Se usa el operador de referencia
+         * para poder usar ya los metodos
+         * definidos en alguna clase statica u objeto
+         * y no tener que crear funciones .
+         *
+         * Para pder utilizar metodos como funciones
+         * Solo deben cumplir con la misma cantidad y tipo
+         * de parametros para poder generar el mismo resultado
+         * */
+        profesores.forEach(System.out::println);
+    }
+
+    static <T> List<T> getList(T...elements){
+        return Arrays.asList(elements);
+    }
+}
